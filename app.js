b7d6e88828758c8a1e6f514733ad8df91cc8eb37
harshita26 <harshitakhandelwal925@gmail.com>
2022-07-05 23:19:33 +0530

    password encrypted

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
@@ -17,6 +18,8 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret:secret, encryptedFields:['password']});
 
 const User= new mongoose.model('User',userSchema);
 
