0794d167ba842a33d0b50cf2ab901937150d05c3
harshita26 <harshitakhandelwal925@gmail.com>
2022-07-05 22:55:57 +0530

    login and register Authentication

---
+++ app.js
@@ -0,0 +1,64 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+const User= new mongoose.model('User',userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/register",function(req,res){
+
+    const user1= new User({email:req.body.username,password:req.body.password});
+    user1.save(function(err){
+        if(!err){
+            res.render("secrets");
+        }else{
+            res.send(err);
+        }
+    });
+});
+
+app.post("/login",function(req,res){
+    User.findOne({email:req.body.username},function(err,foundUser){
+        if(foundUser){
+            if(foundUser.password=== req.body.password){
+                console.log("user found");
+                res.render("secrets");
+            }else{
+                res.send("password not match");
+            }            
+        }else{
+            res.send("user not found");
+        }
+    })
+});
+
+app.listen(3000,function(){
+    console.log("the server is started at 3000");
+});
